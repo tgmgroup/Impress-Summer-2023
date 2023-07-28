1cf18e77bc1f2d2d12d1b58b11b7f2eafb6b241f
George Liu <contact@georgeliu.me>
2023-07-28 09:52:21 +0900

    Added sounds

--- scripts.js
+++ scripts.js
@@ -6,14 +6,14 @@ function PlayAndShow(audioSound, textDiv, audioImage) {
      if(myAudio.paused) {
         myAudio.play();
         myText.style.display = "flex";
-        myButton.src= "nav-images/stop.png";
+        myButton.src= "./nav-images/stop-g55029e04a_1280.png";
         myAudio.onended = ResetPlayAndShow(myAudio, myText, myButton);
      }
     else {
         myAudio.pause();
         myAudio.currentTime = 0;
         myText.style.display = "none";
-        myButton.src= "nav-images/audio.png";
+        myButton.src= "nav-images/play-g79150a13d_1280.png";
   }
   
 } 
@@ -33,4 +33,28 @@ function ResetPlayAndShow(audioSound, textDiv, audioImage) {
     myText.style.display = "none";
     myButton.src= "nav-images/audio.png";
 
+}
+
+
+
+function playHoverSoundOnClass(className) {
+    const elements = document.getElementsByClassName(className);
+    const hoverSound = document.getElementById("hover-sound");
+
+    Array.from(elements).forEach(element => {
+        element.addEventListener("mouseover", () => {
+            hoverSound.play();
+        });
+    });
+}
+
+function playClickSoundOnClass(className) {
+    const elements = document.getElementsByClassName(className);
+    const hoverSound = document.getElementById("click-sound");
+
+    Array.from(elements).forEach(element => {
+        element.addEventListener("click", () => {
+            hoverSound.play();
+        });
+    });
 }
